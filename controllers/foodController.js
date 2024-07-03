878c1ea519f7564834aca2ebca5a8b3aa8df4585
Samuel <samuferfort@gmail.com>
2024-07-03 08:41:56 +0200

    fix layout sort category

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -4,23 +4,24 @@ const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 
 exports.index = asyncHandler(async (req, res, next) => {
-  let [foods, categories] = await Promise.all([Food.find(), Category.find()]);
+  let [foods, categories] = await Promise.all([Food.find(), Category.find().sort("name")]);
 
   if (!categories && !foods) {
     res.status(500).send("An error occurred");
   }
 
   const categoryId = req.params.id;
-
+  let title = "All Categories";
   if (categoryId) {
-    console.log("foods before: ", foods)
-
     foods = foods.filter((food) => food.category == categoryId);
-    console.log("foods after: ", foods)
+    const category = categories.find(
+      (category) => category._id.toString() == categoryId
+    );
+    if (category) title = category.name;
   }
 
   res.render("index", {
-    title: "All Categories",
+    title,
     foods,
     categories,
   });
